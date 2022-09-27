f4ce5f9faeccf32d1626b1ee19d5e3af5f49237e
Jeremy Rilkoff <jrilkoff@gmail.com>
2022-09-26 20:15:06 -0700

    Add files via upload

--- chinook_stretch_exercise.sql
+++ chinook_stretch_exercise.sql
@@ -7,20 +7,39 @@ You can download it here: https://drive.google.com/file/d/0Bz9_0VdXvv9bWUtqM0NBY
 */
 
 
-
 --==================================================================
 /* TASK I
 How many audio tracks in total were bought by German customers? And what was the total price paid for them?
 hint: use subquery to find all of tracks with their prices
 */
 
+SELECT DISTINCT(invoices.BillingCountry), media_types.Name, SUM(invoice_items.Quantity), SUM(invoice_items.UnitPrice)
+    FROM invoice_items
+        JOIN invoices ON invoice_items.InvoiceId = invoices.InvoiceId
+        JOIN tracks ON tracks.TrackId = invoice_items.TrackId
+        JOIN media_types ON media_types.MediaTypeId = tracks.MediaTypeId
+        WHERE invoices.BillingCountry IN 
+            (SELECT customers.Country FROM customers WHERE customers.Country = 'Germany' AND media_types.MediaTypeId != 3)
+    GROUP BY media_types.MediaTypeId;
 
 /* TASK II
-What is the space, in bytes, occupied by the playlist “Grunge”, and how much would it cost?
+What is the space, in bytes, occupied by the playlist “Grunge” # 16, and how much would it cost?
 (Assume that the cost of a playlist is the sum of the price of its constituent tracks).
 */
 
+SELECT pl.Name, SUM(t.Bytes), SUM(t.UnitPrice)
+FROM tracks t
+    JOIN playlist_track pt ON pt.TrackID = t.TrackId
+    JOIN playlists pl ON pl.PlaylistId = pt.PlaylistId
+WHERE pl.Name = 'Grunge'
 
 /* TASK III
-List the names and the countries of those customers who are supported by an employee who was younger than 35 when hired. 
+List the names and the countries of those customers who are supported by an 
+employee who was younger than 35 when hired. 
 */
+
+SELECT * FROM employees;
+
+SELECT c.FirstName, c.LastName, c.Country FROM employees e
+    JOIN customers c ON c.SupportRepId = e.EmployeeId
+WHERE (e.HireDate - e.BirthDate) < 35 AND e.Title LIKE '%SALES%';
